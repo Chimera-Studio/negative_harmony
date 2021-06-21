74719dc6865ca0ed812ed79717840b7bda70ba9a
Dario Dumlijan <dario.dumlijan1@gmail.com>
2021-06-21 20:52:54 +0100

    update

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import AppLoading from "expo-app-loading";
+import { useFonts } from "expo-font";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import MainScreen from "./app/screens/MainScreen";
+
+function App() {
+	let [fontsLoaded] = useFonts({
+		NegativeHarmonyRegular: require("./app/assets/fonts/NegativeHarmony-Montserrat-Regular.ttf"),
+		NegativeHarmonyBold: require("./app/assets/fonts/NegativeHarmony-Montserrat-Bold.ttf"),
+	});
+
+	if (!fontsLoaded) {
+		return <AppLoading />;
+	} else {
+		return <MainScreen />;
+	}
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
